2bea27255324e5dff12f1acfd860a1df1351aad9
masayan382 <masan3821031@gmail.com>
2021-12-29 10:28:12 +0900

    attributes

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@
 
     <x-tests.card title="タイトル１" content="本文１" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル2"></x-tests.card>
+    <x-tests.card title="CSS変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
