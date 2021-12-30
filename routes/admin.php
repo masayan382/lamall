83423a413f2d3ba0644cd52b4153650a298dd892
masayan382 <masan3821031@gmail.com>
2021-12-30 16:10:28 +0900

    owner一覧

--- routes/admin.php
+++ routes/admin.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,6 +25,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+Route::resource('owners', OwnersController::class)
+    ->middleware('auth:admin');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
