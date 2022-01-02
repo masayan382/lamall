76a9446809eb4497470bfe143b15cdd5bf074730
masayan382 <masan3821031@gmail.com>
2022-01-02 09:58:27 +0900

    tinker

---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('shops')->insert([
+            [
+                'owner_id' => 1,
+                'name' => '店名',
+                'information' => '店情報',
+                'filename' => '',
+                'is_selling' => true
+            ],
+            [
+                'owner_id' => 2,
+                'name' => '店名',
+                'information' => '店情報',
+                'filename' => '',
+                'is_selling' => true
+            ],
+
+        ]);
+    }
+}
