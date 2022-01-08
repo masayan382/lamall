0795713b958a71d63b6c193d8992c1dca9b6aee6
masayan382 <masan3821031@gmail.com>
2022-01-08 15:13:02 +0900

    controller modifer

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -14,16 +14,18 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+
         $this->middleware(function ($request, $next) {
-            // dd($request->route()->parameter('shop'));
+            // dd($request->route()->parameter('shop')); //文字列
+            // dd(Auth::id()); //数字
 
             $id = $request->route()->parameter('shop'); //shopのid取得
             if (!is_null($id)) { // null判定
                 $shopsOwnerId = Shop::findOrFail($id)->owner->id;
-                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換 
+                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
                 $ownerId = Auth::id();
                 if ($shopId !== $ownerId) { // 同じでなかったら
-                    abort(404); // 404画面表示 }
+                    abort(404); // 404画面表示
                 }
             }
             return $next($request);
@@ -32,31 +34,37 @@ class ShopController extends Controller
 
     public function index()
     {
-        // $ownerId = Auth::id();
+
+        //$ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
 
-        return view('owner.shops.index', compact('shops'));
+        return view(
+            'owner.shops.index',
+            compact('shops')
+        );
     }
 
     public function edit($id)
     {
         $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
         return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
-        $imageFile = $request->image; //一時保存 
+        $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // Storage::putFile('public/shops', $imageFile);リサイズなしの場合
+            // Storage::putFile('public/shops', $imageFile); リサイズなしの場合
             $fileName = uniqid(rand() . '_');
             $extension = $imageFile->extension();
             $fileNameToStore = $fileName . '.' . $extension;
             $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
             // dd($imageFile, $resizedImage);
 
-            Storage::put('public/shops /' . $fileNameToStore, $resizedImage);
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
+
         return redirect()->route('owner.shops.index');
     }
 }
