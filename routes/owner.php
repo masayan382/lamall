41eb83c60667374365aef24aa6154b9f8b9259f6
masayan382 <masan3821031@gmail.com>
2022-01-09 16:19:43 +0900

    crate image

--- routes/owner.php
+++ routes/owner.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,9 +33,13 @@ Route::prefix('shops')->middleware('auth:owners')->group(function () {
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
 
+Route::resource('images', ImageController::class)
+    ->middleware('auth:owners')->except(['show']);
+
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
 })->middleware(['auth:owners'])->name('dashboard');
+
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
     ->middleware('guest')
