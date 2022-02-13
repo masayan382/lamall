734bbcd1a5dfab4408af5cc579ef70fae97b6eae
masayan382 <masan3821031@gmail.com>
2022-02-13 16:04:56 +0900

    product img

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class ShopSeeder extends Seeder
 {
@@ -18,19 +17,18 @@ class ShopSeeder extends Seeder
         DB::table('shops')->insert([
             [
                 'owner_id' => 1,
-                'name' => '店名',
-                'information' => '店情報',
-                'filename' => '',
-                'is_selling' => true
+                'name' => 'ここに店名が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => 'sample1.jpg',
+                'is_selling' => true    
             ],
             [
                 'owner_id' => 2,
-                'name' => '店名',
-                'information' => '店情報',
-                'filename' => '',
-                'is_selling' => true
+                'name' => 'ここに店名が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
+                'filename' => 'sample2.jpg',
+                'is_selling' => true    
             ],
-
-        ]);
+            ]);
     }
 }
