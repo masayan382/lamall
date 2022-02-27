1ddd450f27cab1875fb0cace4571ad90c84a428e
masayan382 <masan3821031@gmail.com>
2022-02-27 16:18:00 +0900

    add

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -16,25 +16,26 @@ class CartController extends Controller
         $products = $user->products;
         $totalPrice = 0;
 
-        foreach($products as $product){
+        foreach ($products as $product) {
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
         // dd($products, $totalPrice);
 
-        return view('user.cart', 
-            compact('products', 'totalPrice'));
+        return view(
+            'user.cart',
+            compact('products', 'totalPrice')
+        );
     }
 
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
-        ->where('user_id', Auth::id())->first();
+            ->where('user_id', Auth::id())->first();
 
-        if($itemInCart){
+        if ($itemInCart) {
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
-
         } else {
             Cart::create([
                 'user_id' => Auth::id(),
@@ -42,15 +43,14 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        
         return redirect()->route('user.cart.index');
     }
 
     public function delete($id)
     {
         Cart::where('product_id', $id)
-        ->where('user_id', Auth::id())
-        ->delete();
+            ->where('user_id', Auth::id())
+            ->delete();
 
         return redirect()->route('user.cart.index');
     }
