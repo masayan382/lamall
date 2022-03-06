4ec37337d33ab898c9877798f686cea7d6fc5475
masayan382 <masan3821031@gmail.com>
2022-03-06 11:15:18 +0900

    stripe before

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
@@ -16,26 +17,25 @@ class CartController extends Controller
         $products = $user->products;
         $totalPrice = 0;
 
-        foreach ($products as $product) {
+        foreach($products as $product){
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
         // dd($products, $totalPrice);
 
-        return view(
-            'user.cart',
-            compact('products', 'totalPrice')
-        );
+        return view('user.cart', 
+            compact('products', 'totalPrice'));
     }
 
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
-            ->where('user_id', Auth::id())->first();
+        ->where('user_id', Auth::id())->first();
 
-        if ($itemInCart) {
+        if($itemInCart){
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
+
         } else {
             Cart::create([
                 'user_id' => Auth::id(),
@@ -43,15 +43,15 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-
+        
         return redirect()->route('user.cart.index');
     }
 
     public function delete($id)
     {
         Cart::where('product_id', $id)
-            ->where('user_id', Auth::id())
-            ->delete();
+        ->where('user_id', Auth::id())
+        ->delete();
 
         return redirect()->route('user.cart.index');
     }
@@ -60,19 +60,35 @@ class CartController extends Controller
     {
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
-
+        
         $lineItems = [];
-        foreach ($products as $product) {
-            $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
-            array_push($lineItems, $lineItem);
+        foreach($products as $product){
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+            if($product->pivot->quantity > $quantity){
+                return view('user.cart.index');
+            } else {
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);    
+            }
         }
         // dd($lineItems);
+        foreach($products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
+            ]);
+        }
+
+        dd('test');
 
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
@@ -86,9 +102,9 @@ class CartController extends Controller
 
         $publicKey = env('STRIPE_PUBLIC_KEY');
 
-        return view(
-            'user.checkout',
-            compact('session', 'publicKey')
-        );
+        return view('user.checkout', 
+            compact('session', 'publicKey'));
+
+
     }
 }
